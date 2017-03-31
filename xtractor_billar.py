ae61ef037f30a651feb4eefa8eeb955210614dc9
miguelms95 <miguelms.ovi@gmail.com>
2017-04-01 00:41:43 +0200

    printing table

--- xtractor_billar.py
+++ xtractor_billar.py
@@ -20,5 +20,24 @@ def extraerdatos(url):
                     listaDePartidas.append(fila)
                     #print(fila)
         return listaDePartidas
+def extractDataFromTable(list):
+    for item in list:
+        cadena_fila = ''
+        row_content = item.findAll('td')
+        for column in row_content:
+            if(column.find('a') != None):
+                if(column.find('a').find('strong') != None):
+                    cadena_fila += column.find('a').find('strong').string
+                else:
+                    cadena_fila += column.find('a').string
+            elif(column.find('img') != None):
+                try:
+                    cadena_fila += column.find('img').get('alt')
+                    print cadena_fila
+                except TypeError:
+                    cadena_fila += ' '
+        #print '\n'
 
-print(extraerdatos('http://billiardapps.com/drawresults.asp?db=epc&client=&eventnr=2017100&header=no&eventheader=yes&showtabs=yes&tab=Tournament&footer=yes&tournament=W10'))
+print 'Match | Date | Time | D | T | RT | \t \t == Match =='
+extractDataFromTable(extraerdatos('http://billiardapps.com/drawresults.asp?db=epc&client=&eventnr=2017100&header=no&eventheader=yes&showtabs=yes&tab=Tournament&footer=yes&tournament=W10'))
+#print(extraerdatos('http://billiardapps.com/drawresults.asp?db=epc&client=&eventnr=2017100&header=no&eventheader=yes&showtabs=yes&tab=Tournament&footer=yes&tournament=W10'))
